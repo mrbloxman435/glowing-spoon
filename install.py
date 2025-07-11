0b44428110361079c1568999a2a1b37461325145
mrbloxman435 <yeeyeeepeic414@gmail.com>
2025-07-10 17:32:04 -0700

    calc is slang for calculator

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import launch
 
 # TODO: add pip dependency if need extra module only on extension
-
+launch.run("calc")
 # if not launch.is_installed("aitextgen"):
 #     launch.run_pip("install aitextgen==0.6.0", "requirements for MagicPrompt")
